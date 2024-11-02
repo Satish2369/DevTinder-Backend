78a694799a2e1e11a25a2669f6a13f3023352cb8
satish2369 <satishmourya410@gmail.com>
2024-11-02 13:17:32 +0530

    backend

---
+++ src/app.js
@@ -0,0 +1,18 @@
+console.log("backend project started")
+
+const express = require('express');
+
+const app = express();
+
+app.use( "/test",(req,res)=>{
+    res.send("hello world");
+})
+
+app.use( "/first",(req,res)=>{ 
+    res.send("hello coder hello              bhai");
+})
+
+
+app.listen(3000,()=>{
+    console.log("Server is successfully listening on port on 3000")
+})
