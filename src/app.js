4cdde6f307520dbe9083966e456daeda41f1a89a
satish2369 <satishmourya410@gmail.com>
2025-01-14 16:43:54 +0530

    updation deletion

--- src/app.js
+++ src/app.js
@@ -14,7 +14,6 @@ app.post('/signup', async (req,res)=>{
 
  //  console.log(req.body);
 
-    
 // creating a new instance of the new user model
     const user = new User(req.body);
 
@@ -28,11 +27,129 @@ catch (err) {
 
 })
 
+
+app.get("/user", async (req,res)=>{
+
+
+    const userEmail = req.body.emailId;
+    
+    try{
+       const user = await User.find({emailId:userEmail});
+
+
+       if(user.length===0){
+           res.status(404).send("user not found")
+       }
+       else{
+        res.send(user);
+       }
+       
+    }
+    catch (err) {
+        res.status(400).send("something went wrong:" + err.message)
+    }
+   
+
+
+});
+
+
+
+
+
+//get all the users from the users database
+ app.get("/feed",async(req,res)=>{
+
+    const userEmail = req.body.emailId;
+try{
+  const user=  await User.findOne({emailId:userEmail});
+
+  if(!user){
+    res.status(404).send("user not found")
+  }
+  res.send(user);
+}
+ catch (err) {
+        res.status(400).send("something went wrong:" + err.message)
+    }
+   
+
+
+ });
+
+ app.get("/practiseId", async (req, res) => {
+    const Id = req.body._id;
+
+    if (!Id) {
+        return res.status(400).send("Invalid id");
+    }
+
+    try {
+        const user = await User.findById(Id);
+
+        if (!user) {
+            return res.status(404).send("User not found");
+        }
+
+        res.send(user);
+    } catch (e) {
+        // Check if the error is related to invalid ObjectId
+        if (e.kind === 'ObjectId') {
+            res.status(400).send("Invalid id format");
+        } else {
+            res.status(500).send("Something went wrong");
+        }
+    }
+});
+
+
+
+app.delete("/user",async(req,res)=>{
+    const userId = req.body.userId;
+
+
+    try{
+        const user = await User.findByIdAndDelete({_id:userId});
+        console.log("user deleted")
+        res.send("user deleted successfully")
+
+    }
+
+    catch (err) {
+        res.status(400).send("something went wrong:" + err.message)
+    }
+})
+
+
+
+
+app.patch("/user",async (req,res)=>{ 
+
+    const userId = req.body.userId;
+    const data= req.body;
+
+    try{
+      const user =   await User.findByIdAndUpdate({_id:userId},data,{runValidators:true});
+      
+      console.log(user)
+        res.send("user updated successfully");
+
+
+    }
+
+    catch (err) {
+        res.status(400).send("something went wrong:" + err.message)
+    }
+})
+
+
+
+
 connectDB().then(()=>{
     console.log("connection established")
     
-app.listen(7000,()=>{
-    console.log("Server is successfully listening on port on 7777")
+app.listen(3000,()=>{
+    console.log("Server is successfully listening on port on 3000")
 })
 }).catch(err=>console.error("database cannot be cpnnected"))
 
